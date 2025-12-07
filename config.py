3092d39c208dd58c24b3308471d8d43797a2a934
Daniel Almeida <47141490+dcalmeida149@users.noreply.github.com>
2025-12-07 20:56:38 +0000

    mudança para sqlite

--- config.py
+++ config.py
@@ -1,16 +1,13 @@
 
 import json
+import os
 
 with open('secrets.json') as f:
     secrets = json.load(f)
 
 class Config:
-    SQLALCHEMY_DATABASE_URI = secrets['DATABASE_URI']
+    # SQLite database file in the project root
+    basedir = os.path.abspath(os.path.dirname(__file__))
+    SQLALCHEMY_DATABASE_URI = f'sqlite:///{os.path.join(basedir, "database.db")}'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
-    SQLALCHEMY_ENGINE_OPTIONS = {
-        'pool_size': 10,
-        'max_overflow': 20,
-        'pool_timeout': 30,
-        'pool_recycle': 1800,
-    }
     APP_SECRET = secrets['APP_SECRET']
